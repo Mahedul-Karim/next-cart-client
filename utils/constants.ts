a6055c6a60613d9594bf96a23d39c99099bf76ec
Mahedul-Karim <karimrupo21@gmail.com>
2025-12-18 16:02:42 +0600

    minor bug fix

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4000/api/v1";
-// export const BASE_URL = "https://next-cart-server.vercel.app/api/v1";
+// export const BASE_URL = "http://localhost:4000/api/v1";
+export const BASE_URL = "https://next-cart-server.vercel.app/api/v1";
 export const ITEMS_PER_PAGE = 10;
